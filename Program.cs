e522017b087863944db37a92d9ee0f88e21b6ad5
Martin Honnen <martin.honnen@gmx.de>
2023-02-09 09:07:09 +0100

    Update to Saxon HE 11.5 and IKVM Maven 1.4.1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿using net.sf.saxon.s9api;
 using net.liberty_development.SaxonHE11s9apiExtensions;
-using System.Reflection;
-using org.w3c.dom.xpath;
+//using System.Reflection;
 
-// force loading of updated xmlresolver
-ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver"));
-ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver_data"));
+// force loading of updated xmlresolver (no longer necessary for Saxon HE 11.5)
+//ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver"));
+//ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver_data"));
 
 var processor = new Processor(false);
 
